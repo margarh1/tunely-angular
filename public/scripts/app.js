be806d2808dbf358ce1818dad544fe2a1c0be88f
margarh1 <ha.margaret1@gmail.com>
2016-11-29 12:02:50 -0800

    finished sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,27 +8,56 @@ angular
   .module('tunely', [])
   .controller('AlbumsIndexController', AlbumsIndexController);
 
-function AlbumsIndexController() {
+function AlbumsIndexController($http) {
   var vm = this;
   vm.newAlbum = {};
 
   vm.newAlbum = {
-    name: 'License to Ill',
+    name: 'Licensed to Ill',
     artistName: 'Beastie Boys'
   };
 
-  vm.albumsList = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+  vm.albumsList
+
+  $http({
+    method: 'GET',
+    url: '/api/albums'
+  }).then(onGetSuccess, onError);
+
+  vm.createAlbum = function() {
+    console.log(vm.newAlbum)
+    $http({
+      method: 'POST',
+      url: '/api/albums',
+      data: vm.newAlbum
+    }).then(onPostSuccess, onError);
+  };
+
+  function onGetSuccess(albums) {
+    vm.albumsList = albums.data;
+  };
+
+  function onPostSuccess(album) {
+    vm.albumsList.push(album.data);
+    vm.newAlbum = {};
+  };
+
+  function onError(err) {
+    console.log(err)
+  };
+
+  // vm.albumsList = [
+  //   {
+  //     name: 'Coming Home',
+  //     artistName: 'Leon Bridges'
+  //   },
+  //   {
+  //     name: 'Are We There',
+  //     artistName: 'Sharon Van Etten'
+  //   },
+  //   {
+  //     name: 'The Queen is Dead',
+  //     artistName: 'The Smiths'
+  //   }
+  // ];
 }
