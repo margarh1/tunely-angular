b7421e363b3e35092aa6331d80172e5934b903d4
margarh1 <ha.margaret1@gmail.com>
2016-11-29 12:36:50 -0800

    finished sprint 3, styled buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,7 +25,6 @@ function AlbumsIndexController($http) {
   }).then(onGetSuccess, onError);
 
   vm.createAlbum = function() {
-    console.log(vm.newAlbum)
     $http({
       method: 'POST',
       url: '/api/albums',
@@ -33,14 +32,39 @@ function AlbumsIndexController($http) {
     }).then(onPostSuccess, onError);
   };
 
+  vm.deleteAlbum = function(album) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/' + album._id,
+    }).then(onDeleteSuccess, onError);
+  };
+
+  vm.editAlbum = function(album) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/' + album._id,
+      data: album
+    }).then(onEditSuccess, onError);
+  };
+
   function onGetSuccess(albums) {
     vm.albumsList = albums.data;
   };
 
-  function onPostSuccess(album) {
-    vm.albumsList.push(album.data);
+  function onPostSuccess(newAlbum) {
+    vm.albumsList.push(newAlbum.data);
     vm.newAlbum = {};
   };
+
+  function onDeleteSuccess(deletedAlbum) {
+    var deletedAlbumIdx = vm.albumsList.indexOf(deletedAlbum);
+    vm.albumsList.splice(deletedAlbumIdx, 1);
+  };
+
+  function onEditSuccess(editedAlbum) {
+    var editedAlbumIdx = vm.albumsList.indexOf(editedAlbum.config.data);
+    vm.albumsList.splice(editedAlbumIdx, 1, editedAlbum.data);
+  }
 
   function onError(err) {
     console.log(err)
